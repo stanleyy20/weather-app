06f2ecdcc981b5f35a9e0e79bc5a58ef6c33129c
Dawid Stach <dstach20@gmail.com>
2022-02-16 12:16:25 +0100

    edit

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,6 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Result from './Result';
 import Form from './Form';
-import DayTimeWeather from './DayTimeWeather';
-import { Route, Routes, useNavigate } from 'react-router-dom';
-import { cleanup } from '@testing-library/react';
 
 // Klucz do API
 const APIKey = `d5ef99401ff5b94d28e426238d4d9881`;
@@ -23,12 +20,9 @@ const App = () => {
   const [description, setDecryption] = useState('');
   const [img, setImg] = useState([]);
   const [timezone, setTimezone] = useState(0);
-  const [lon, setLon] = useState('');
-  const [lat, setLat] = useState('');
-  const [dayliWeather, setDayliWeather] = useState('');
-  const [isResultActive, setIsResultActive] = useState(false);
-
-  const navigate = useNavigate();
+  // const [lon, setLon] = useState('');
+  // const [lat, setLat] = useState('');
+  // const [daily, setDaily] = useState([]);
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
@@ -36,7 +30,6 @@ const App = () => {
 
   useEffect(() => {
     const API = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${APIKey}&units=metric&lang=${PL_LANG}`;
-    const APISecond = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,current,minutely,alerts&appid=${APIKey}&units=metric&lang=${PL_LANG}`;
     fetch(API)
       .then((response) => {
         if (response.ok) {
@@ -58,83 +51,55 @@ const App = () => {
         setDecryption(data.weather[0].description);
         setImg(data.weather[0].icon);
         setTimezone(data.timezone);
-        setLat(data.coord.lat);
-        setLon(data.coord.lon);
-        setIsResultActive(true);
+        // setLat(data.coord.lat);
+        // setLon(data.coord.lon);
       })
       .catch((error) => {
         console.log(error);
         setCity(inputValue);
         setError(true);
       });
+  }, [inputValue]);
 
-    fetch(APISecond)
-      .then((response) => {
-        if (response.ok) {
-          return response;
-        }
-        throw Error('Nie udało się wczytać danych');
-      })
-      .then((response) => response.json())
-      .then((data) => {
-        setError(false);
-        setDayliWeather(data.daily);
-      })
-      .catch((error) => {
-        console.log(error);
-        setError(true);
-      });
-    return () => {
-      cleanup();
-    };
-  }, [inputValue, lat, lon]);
-
-  const handleOnClickDown = () => {
-    setIsResultActive((prev) => !prev);
-    navigate('/dayli');
-  };
-
-  const handleClickBack = () => {
-    navigate('/');
-    setIsResultActive((prev) => !prev);
-  };
+  // useEffect(() => {
+  //   const API = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,current,minutely,alerts&appid=${APIKey}&units=metric&lang=${PL_LANG}`;
+  //   fetch(API)
+  //     .then((response) => {
+  //       if (response.ok) {
+  //         return response;
+  //       }
+  //       throw Error('Nie udało się wczytać danych');
+  //     })
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setError(false);
+  //       setDaily(data.daily);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //       setError(true);
+  //     });
+  // }, [lat]);
 
   return (
     <div className='App'>
       <h1>Aplikacja pogodowa</h1>
       <Form value={inputValue} change={handleInputChange} />
-      <Routes>
-        <Route
-          path='/'
-          element={
-            <Result
-              date={date}
-              city={city}
-              sunrise={sunrise}
-              sunset={sunset}
-              pressure={pressure}
-              wind={wind}
-              temp={temp}
-              error={error}
-              inputValue={inputValue}
-              img={img}
-              description={description}
-              timezone={timezone}
-              isResultActive={isResultActive}
-              handleOnClickDown={handleOnClickDown}
-            />
-          }
-        />
-        <Route
-          path='/dayli'
-          element={
-            <DayTimeWeather
-              handleClickBack={handleClickBack}
-              dayliWeather={dayliWeather}
-            />
-          }
-        />
-      </Routes>
+      <Result
+        date={date}
+        city={city}
+        sunrise={sunrise}
+        sunset={sunset}
+        pressure={pressure}
+        wind={wind}
+        temp={temp}
+        error={error}
+        inputValue={inputValue}
+        img={img}
+        description={description}
+        timezone={timezone}
+        // daily={daily}
+      />
     </div>
   );
 };
--- src/components/Result.js
+++ src/components/Result.js
@@ -14,8 +14,6 @@ const Result = ({
   img,
   description,
   timezone,
-  isResultActive,
-  handleOnClickDown,
 }) => {
   let content = null;
 
@@ -29,23 +27,19 @@ const Result = ({
 
     const weatherIcon = `http://openweathermap.org/img/wn/${img}@2x.png`;
 
-    content = isResultActive ? (
+    content = (
       <React.Fragment>
-        <div className='result'>
-          <h3> {date}</h3>
-          <img src={weatherIcon} alt='weather-icon' />
-          <h3>{description}</h3>
-          <h4 className='temp'>{Math.round(temp)} &#176;C</h4>
-          <h4>Wschód słońca: {sunriseTime}</h4>
-          <h4>Zachód słońca: {sunsetTime}</h4>
-          <h4>Ciśnienie: {pressure} hPa</h4>
-          <h4>Prędkość wiatru: {wind} m/s</h4>
-        </div>
-        <button onClick={handleOnClickDown}>Prognoza na 7 dni</button>
+        <h3> {date}</h3>
+        <img src={weatherIcon} alt='weather-icon' />
+        <h3>{description}</h3>
+        <h4 className='temp'>{Math.round(temp)} &#176;C</h4>
+        <h4>Wschód słońca: {sunriseTime}</h4>
+        <h4>Zachód słońca: {sunsetTime}</h4>
+        <h4>Ciśnienie: {pressure} hPa</h4>
+        <h4>Prędkość wiatru: {wind} m/s</h4>
       </React.Fragment>
-    ) : null;
+    );
   }
-
   let result = error ? <h3>Nie mamy w bazie {city}</h3> : content;
 
   if (inputValue.length === 0) {
@@ -53,7 +47,7 @@ const Result = ({
     return <div></div>;
   }
 
-  return result;
+  return <div className='result'>{result}</div>;
 };
 
 export default Result;
